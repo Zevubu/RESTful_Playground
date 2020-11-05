cd722eca7d8d61360b1572efa232dacf99b9f70e
Zev Hoffman <zevubu@gmail.com>
2020-11-05 15:22:01 -0800

    built out basic sql connection

--- server/index.js
+++ server/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 app.get('/', (req,res) => res.send('Hello world!'));
+app.all('/ping', (req, res) => res.send(`${new Date()}`))
 
 app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}`))
